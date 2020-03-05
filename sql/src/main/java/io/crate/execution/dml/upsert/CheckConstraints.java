63830f0f1a67fb823b9b5a423d7bfe208ff375ca
marregui <miguel.arregui@gmail.com>
2020-03-05 16:00:02 +0100

    No joda format and add inline comment to avoid confusion (this null check is intended)

--- sql/src/main/java/io/crate/execution/dml/upsert/CheckConstraints.java
+++ sql/src/main/java/io/crate/execution/dml/upsert/CheckConstraints.java
@@ -87,7 +87,9 @@ public final class CheckConstraints<T, E extends CollectExpression<T, ?>> {
             Tuple<? extends Input<?>, CheckConstraint<Symbol>> checkEntry = checkConstraints.get(i);
             Input<?> checkInput = checkEntry.v1();
             Boolean value = (Boolean) checkInput.value();
-            if (null == value) {
+            if (value == null) {
+                // most expressions evaluate to null if any operand is null.
+                // a check expression will evaluate to true in these cases.
                 continue;
             }
             if (!value.booleanValue()) {
